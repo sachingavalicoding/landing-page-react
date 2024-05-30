50d9f89da147ac5d7055903638c7c7d5a7963831
Sachin Gavali <sachingavali321@gmail.com>
2024-05-30 21:08:47 +0530

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-/* eslint-disable react/no-unknown-property */
-import video1 from "/video1.mp4";
-import video2 from "/video2.mp4";
+
 const App = () => {
   return (
     <>
-      <div>
-        <video autoPlay controls loop muted src={video1}></video>
-        <video autoPlay controls loop muted src={video2}></video>
-       
-      </div>
+    
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
