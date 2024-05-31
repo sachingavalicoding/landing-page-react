3d5b0986a506f5fb5388bf066b6e93f7579206f7
Sachin Gavali <sachingavali321@gmail.com>
2024-05-31 14:51:56 +0530

    testimonils and pricing section ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-
+import Pricing from "./Components/Pricing"
 import Navbar from "./Components/Navbar"
 import Footer from "./Components/Footer"
 import HeroSection from "./Components/HeroSection"
 import Feature from "./Components/Feature"
+import About from "./Components/About"
 const App = () => {
   return (
     <>
     <Navbar />
     <HeroSection />
     <Feature />
+    <About />
+    <Pricing />
     <Footer />
     </>
   )
--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -1,7 +1,7 @@
 import { features } from "../data/data"
 const Feature = () => {
   return (
-    <section className="w-full mt-16 border-2  py-14">
+    <section className="w-full mt-16  py-14">
         <h4 className="text-xl text-mainColor  text-center "> FEATURE </h4>
         <h2 className="text-5xl text-center mt-8 tracking-wider "> Easily build <span className="text-mainColor "> your code </span></h2>
           <div  className=" xl:w-[1080px] w-full xl:px-0 px-8 mx-auto grid grid-cols-1  place-items-center sm:grid-cols-2 lg:grid-cols-3 gap-4 items-center mt-10 ">
